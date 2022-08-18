05e9a763f5eb77317234cd7e46766f3c3e9abc02
YongJin Jung <jyj1641@gmail.com>
2022-08-18 10:55:31 +0900

    단순하고 실용적인 컨트롤러 - v4

--- src/main/java/hello/servlet/web/frontcontroller/v4/ControllerV4.java
+++ src/main/java/hello/servlet/web/frontcontroller/v4/ControllerV4.java
@@ -1,5 +1,7 @@
 package hello.servlet.web.frontcontroller.v4;
 
+import hello.servlet.web.frontcontroller.ModelView;
+
 import java.util.Map;
 
 public interface ControllerV4 {
@@ -12,5 +14,5 @@ public interface ControllerV4 {
      */
     String process(Map<String, String> paramMap, Map<String, Object> model);
 
-    default String process(Map<String, String> paramMap){return null;}
+    default ModelView process(Map<String, String> paramMap){return null;}
 }
